1238351d48b158f6f64de14daec06c35c5ddf211
admin <monstriksh@gmail.com>
2022-11-29 18:17:48 +0500

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
+admin.site.site_title = "Admin panel"
+admin.site.site_header = "Admin panel"
+admin.site.index_title = "Admin panel"
+
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', views.post_list, name='post_list'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
